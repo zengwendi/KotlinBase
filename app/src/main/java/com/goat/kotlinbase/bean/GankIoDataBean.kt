ce31b2822f7d46b5e6999ced7093151f1586f99c
ADMIN\Administrator <zeng_wendi@163.com>
2017-06-15 21:21:25 +0800

    把bean类改成数据类

--- app/src/main/java/com/goat/kotlinbase/bean/GankIoDataBean.kt
+++ app/src/main/java/com/goat/kotlinbase/bean/GankIoDataBean.kt
@@ -7,55 +7,18 @@ import java.io.Serializable
  * Created by jingbin on 2016/11/24.
  */
 
-class GankIoDataBean : Serializable {
+data class GankIoDataBean constructor(val isError: Boolean = false, val results: List<ResultBean>? = null) : Serializable {
 
-    val isError: Boolean = false
-    /**
-     * _id : 5832662b421aa929b0f34e99
-     * createdAt : 2016-11-21T11:12:43.567Z
-     * desc :  深入Android渲染机制
-     * publishedAt : 2016-11-24T11:40:53.615Z
-     * source : web
-     * type : Android
-     * url : http://blog.csdn.net/ccj659/article/details/53219288
-     * used : true
-     * who : Chauncey
-     */
+    data class ResultBean(val _id: String? = null,
+                          val createdAt: String? = null,
+                          val desc: String? = null,
+                          val publishedAt: String? = null,
+                          val source: String? = null,
+                          val type: String? = null,
+                          val url: String? = null,
+                          val isUsed: Boolean = false,
+                          val who: String? = null,
+                          val images: List<String>? = null) : Serializable {
 
-    val results: List<ResultBean>? = null
-
-    class ResultBean : Serializable {
-
-        val _id: String? = null
-        val createdAt: String? = null
-        val desc: String? = null
-        val publishedAt: String? = null
-        val source: String? = null
-        val type: String? = null
-        val url: String? = null
-        val isUsed: Boolean = false
-        val who: String? = null
-        val images: List<String>? = null
-
-        override fun toString(): String {
-            return "ResultsBean{" +
-                    "who='" + who + '\'' +
-                    ", used=" + isUsed +
-                    ", url='" + url + '\'' +
-                    ", type='" + type + '\'' +
-                    ", source='" + source + '\'' +
-                    ", publishedAt='" + publishedAt + '\'' +
-                    ", desc='" + desc + '\'' +
-                    ", createdAt='" + createdAt + '\'' +
-                    ", _id='" + _id + '\'' +
-                    '}'
-        }
-    }
-
-    override fun toString(): String {
-        return "GankIoDataBean{" +
-                "error=" + isError +
-                ", results=" + results +
-                '}'
     }
 }
